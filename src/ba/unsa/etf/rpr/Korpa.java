2b9bcf955465f9e630a10ab49386c2ff9a038acd
Azra Ahmic <aahmic3@etf.unsa.ba>
2020-10-30 19:53:24 +0100

    INITIAL COMMIT

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,28 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] korpa=new Artikl[50];
+    public boolean dodajArtikl(Artikl art)
+    {
+        for(int i=0; i<50; i++)
+        {
+            if(a[i]==null)
+            {
+                a[i]=art;
+                return true;
+            }
+        }
+        return false;
+    }
+    public void izbaciArtiklSaKodom(String kod) {
+        Artikl pomocniartikl;
+        for (int i = 0; i < 50; i++) {
+            if (korpa[i].getKod() == kod) {
+                while (korpa[i] != null) {
+                    korpa[i] = korpa[i + 1];
+                    i++;
+                }
+            }
+        }
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artiklisupermarketa=new Artikl[1000];
+
+    public boolean dodajArtikl(Artikl artikl)
+    {
+        for(int i=0; i<50; i++)
+        {
+            if(artiklisupermarketa[i]==null)
+            {
+                artiklisupermarketa[i]=artikl;
+                return true;
+            }
+        }
+            return false;
+    }
+
 }
+
+
+
